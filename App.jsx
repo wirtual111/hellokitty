2107d385b2e6c349fb8ecbb92e6b32b3d3040610
wirtual111 <adityarajvanshi2020@gmail.com>
2026-05-28 23:00:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{padding: "40px", fontFamily: "Arial"}}>
+      <h1>🎀 KittyLearn</h1>
+      <p>Hello Kitty themed education website.</p>
+    </div>
+  )
+}
